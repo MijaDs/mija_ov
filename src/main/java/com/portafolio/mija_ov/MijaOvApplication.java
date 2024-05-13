56c3d554a0f35bade02c187f3696158bcaea8773
MijaDs <rmhborsig@gmail.com>
2024-05-13 13:42:43 -0600

    first commit

---
+++ src/main/java/com/portafolio/mija_ov/MijaOvApplication.java
@@ -0,0 +1,13 @@
+package com.portafolio.mija_ov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MijaOvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MijaOvApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/portafolio/mija_ov/MijaOvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portafolio.mija_ov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MijaOvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
